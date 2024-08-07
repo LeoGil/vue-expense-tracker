74a59c819db6f18e7cf6cb652359ed379f09fe3c
Leonardo Gil <leogilc@gmail.com>
2024-08-07 00:09:17 -0300

    ajustes no visual

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,11 +3,11 @@
     <form>
         <div>
             <label class="inline-block mx-0 my-3 font-semibold" for="text">Name</label>
-            <input class="border border-gray-300 rounded-sm block text-xs p-2 w-full" type="text" id="text">
+            <input class="border border-gray-300 rounded block text-xs p-2 w-full" type="text" id="text">
         </div>
         <div>
             <label class="inline-block mx-0 my-3 font-semibold" for="amount">Amount</label>
-            <input class="border border-gray-300 rounded-sm block text-xs p-2 w-full" type="number" id="amount">
+            <input class="border border-gray-300 rounded block text-xs p-2 w-full" type="number" id="amount">
         </div>
         <button
             class="transition-all duration-100 cursor-pointer bg-blue-500 hover:bg-blue-600 text-white font-bold p-2 rounded shadow-md w-full mt-3 block mb-8">
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,15 @@
 <template>
     <h3 class="text-lg font-bold border-b-2 border-black pb-1 mt-10 mx-0 mb-3">History</h3>
     <ul class="list-none p-0 mb-10">
-        <li class="bg-white shadow-md text-gray-600 flex justify-between relative p-3 mx-0 my-3 border-r-4 border-red-600">
-            Cash <span>-R$ 400,00</span><button>X</button>
+        <li
+            class="bg-white shadow-md text-gray-600 flex justify-between relative p-3 mx-0 my-3 border-r-4 border-red-600 font-bold rounded">
+            Cash <span>-R$ 400,00</span>
+            <button class="cursor-pointer border-0 text-slate-200 text-xl leading-5 px-5 py-0.5 absolute top-2/4 left-0 -translate-x-full -translate-y-1/2 opacity-100 hover:opacity-100 hover:text-red-600 easy-in duration-200">X</button>
         </li>
-        <li class="bg-white shadow-md text-gray-600 flex justify-between relative p-3 mx-0 my-3 border-r-4 border-green-600">
-            Paycheck <span>R$ 400,00</span><button>X</button>
+        <li
+            class="bg-white shadow-md text-gray-600 flex justify-between relative p-3 mx-0 my-3 border-r-4 border-green-600 font-bold rounded">
+            Paycheck <span>R$ 400,00</span>
+            <button class="cursor-pointer border-0 text-slate-200 text-xl leading-5 px-5 py-0.5 absolute top-2/4 left-0 -translate-x-full -translate-y-1/2 opacity-100 hover:opacity-100 hover:text-red-600 easy-in duration-200">X</button>
         </li>
     </ul>
 </template>
