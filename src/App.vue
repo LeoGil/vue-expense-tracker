ee982fb7afe5a8a58fefa4e1ddd2f2dfe8267420
Leonardo Gil <leogilc@gmail.com>
2024-08-08 00:58:18 -0300

    atualizando valor total

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       </div>
       <div>
         <div>
-          <BalanceComponent />
+          <BalanceDisplayComponent :balance="balance" />
         </div>
         <IncomeExpensesComponent />
         <TransactionListComponent :transactions="transactions" />
@@ -18,17 +18,23 @@
 
 <script setup>
 import HeaderComponent from './components/Header.vue'
-import BalanceComponent from './components/Balance.vue'
+import BalanceDisplayComponent from './components/BalanceDisplay.vue'
 import IncomeExpensesComponent from './components/IncomeExpenses.vue'
 import TransactionListComponent from './components/TransactionList.vue'
 import AddTransactionComponent from './components/AddTransaction.vue'
 
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const transactions = ref([
-    { id: 1, text: 'Cash', amount: -400 },
-    { id: 2, text: 'Paycheck', amount: 400 },
-    { id: 3, text: 'Food', amount: -100 },
-    { id: 4, text: 'Dividends', amount: 1000 }
-])
+  { id: 1, text: 'Cash', amount: -400 },
+  { id: 2, text: 'Paycheck', amount: 400 },
+  { id: 3, text: 'Food', amount: -100 },
+  { id: 4, text: 'Dividends', amount: 1000 }
+]);
+
+const balance = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
+})
 </script>
---
+++ src/components/BalanceDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3 class="text-lg font-bold border-b-2 border-black pb-1 mt-10 mx-0 mb-3">Balance</h3>
+  <h1 class="text-4xl font-bold">R$ {{ balance }}</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
