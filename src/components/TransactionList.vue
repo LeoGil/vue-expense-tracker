685073accc8338f271f0a64a778e2feafa6c6a71
Leonardo Gil <leogilc@gmail.com>
2024-08-09 00:51:37 -0300

    teste caso nao tenha transaction

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,6 +1,6 @@
 <template>
     <h3 class="text-lg font-bold border-b-2 border-black pb-1 mt-10 mx-0 mb-3">History</h3>
-    <ul class="list-none p-0 mb-10">
+    <ul v-if="transactions.length" class="list-none p-0 mb-10">
         <li 
             class="bg-white shadow-md text-gray-600 flex justify-between relative p-3 mx-0 my-3 border-r-4 font-bold rounded"
             v-for="transaction in transactions" :key="transaction.id"
@@ -10,6 +10,9 @@
             <button @click="deleteTransaction(transaction.id)" class="cursor-pointer border-0 text-slate-200 text-xl leading-5 px-5 py-0.5 absolute top-2/4 left-0 -translate-x-full -translate-y-1/2 opacity-100 hover:opacity-100 hover:text-red-600 easy-in duration-200">X</button>
         </li>
     </ul>
+    <div v-else class="mt-4 mx-0 mb-3 text-center">
+        <i>No Transactions</i>
+    </div>
 </template>
 
 <script setup>
